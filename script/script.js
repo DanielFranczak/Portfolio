2ff69486749992caba15caf8d13103623a3edcf7
DanielFranczak <dan.franczak@gmail.com>
2018-09-13 21:45:59 +0200

    Add  another files and title for section my values

---
+++ script/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    //navbar-toggler
+    $('.navbar-toggler').click(function(){
+        $('.navbar-toggler').toggleClass('change');
+    })
+    //adding background and fixed
+    $(window).scroll(function(){
+        let position = $(this).scrollTop();
+        if(position  >= 142){
+            $('.navbar').addClass('navbar-background');
+            $('.navbar').addClass('fixed-top');
+        }
+        else{
+            $('.navbar').removeClass('navbar-background');
+            $('.navbar').removeClass('fixed-top');
+        }
+    })
+    //smooth scrool
+    $('.nav-item a').click(function(link){
+    link.preventDefault();
+    let target = $(this).attr('href');
+    $('html, body').stop().animate({
+        scrollTop: $(target).offset().top
+    }, 1000);
+    
+    })
+    // init Isotope
+    let $grid = $('.grid').isotope({
+        // options
+    });
+    // filter items on button click
+    $('.filter-button-group').on('click', 'button', function () {
+        let filterValue = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: filterValue
+        });
+    });
+    // layout Isotope after each image loads
+$grid.imagesLoaded().progress( function() {
+    $grid.isotope('layout');
+  });
+});
